42909154e1ef0aad5aad2dcf455deb77d7301713
joanne <jojoannexo@gmail.com>
2015-05-13 11:13:24 +0530

    hours_ahead and hours_behind

--- project1/urls.py
+++ project1/urls.py
@@ -1,5 +1,5 @@
 from django.conf.urls import patterns, include, url
-from project1.views import hello, current_datetime
+from project1.views import hello, current_datetime, hours_ahead, hours_behind
 
 # Uncomment the next two lines to enable the admin:
 # from django.contrib import admin
@@ -9,6 +9,8 @@ urlpatterns = patterns('',
 	('^hello/$', hello),
 	('^$', hello),
 	('^time/$', current_datetime), 
+	(r'^time/plus/(\d{1,2})/$', hours_ahead),
+	(r'^time/minus/(\d{1,2})/$', hours_behind),
     # Examples:
     # url(r'^$', 'project1.views.home', name='home'),
     # url(r'^project1/', include('project1.foo.urls')),
--- project1/views.py
+++ project1/views.py
@@ -1,4 +1,4 @@
-from django.http import HttpResponse
+from django.http import Http404, HttpResponse
 import datetime 
 
 def hello(request): 
@@ -8,3 +8,21 @@ def current_datetime(request):
 	now = datetime.datetime.now()
 	html = "<html><body> It is now %s.</body></html>" % now
 	return HttpResponse(html) 
+
+def hours_ahead(request, offset):
+	try:
+		offset = int(offset)
+	except ValueError:
+		raise Http404()
+	dt = datetime.datetime.now() + datetime.timedelta(hours=offset)
+	html ="<html><body>In %s hour(s), it will be %s <3 </body></html>" % (offset, dt) 
+	return HttpResponse(html)
+
+def hours_behind(request, offset):
+	try:
+		offset = int(offset)
+	except ValueError:
+		raise Http404()
+	dt = datetime.datetime.now() - datetime.timedelta(hours=offset)
+	html="<html><body> %s hours ago, it was %s <3 </body> </html>" % (offset, dt) 
+	return HttpResponse(html)
